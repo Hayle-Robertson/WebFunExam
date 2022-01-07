cebd0e38c0b9b45906465d681590864321403c3a
Hayle Robertson <Haylerobertson@gmail.com>
2022-01-07 10:04:19 -0600

    first commit

---
+++ script.js
@@ -0,0 +1,11 @@
+function over(){
+    document.querySelector("#mainimage").setAttribute('src', 'images/assets/succulents-2.jpg');
+}
+
+function out(){
+    document.querySelector("#mainimage").setAttribute('src', 'images/assets/succulents-1.jpg');
+}
+
+function Hide(){
+    document.querySelector('#policy').remove();
+}
